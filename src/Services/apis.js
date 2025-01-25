8afa84aa067e772fd4d0de8ad4afce2c99719ac9
Abhay Keshari <instantboostbyabhaykeshari>
2025-01-25 15:50:11 +0530

    abhay

---
+++ src/Services/apis.js
@@ -0,0 +1,9 @@
+const baseUrl = "http://localhost:4000/api/v1";
+
+export const auth = {
+    SIGNUP_API: `${baseUrl}/signUp`
+}
+
+export const order = {
+    ALL_ORDER: `${baseUrl}/order/allOrder`
+}
